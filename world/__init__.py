296f9eba89ed67da3d0429b242107d60010a24b9
adamszelestey-se <adam.szelestey@se.com>
2026-05-01 10:39:07 +0200

    Simple manhattan distance based reward function added

--- world/__init__.py
+++ world/__init__.py
@@ -3,10 +3,18 @@ from pathlib import Path
 from world.grid import Grid
 from world.gui import GUI
 from world.environment import Environment
+from world.rewards import build_manhattan_reward_function, find_target_position
 
 
 GRID_CONFIGS_FP = Path(__file__).parents[1].resolve() / Path("grid_configs")
 GRID_CONFIGS_FP.mkdir(parents=True, exist_ok=True)
 
-__all__ = ["GRID_CONFIGS_FP", "Grid", "GUI", "Environment"]
+__all__ = [
+    "GRID_CONFIGS_FP",
+    "Grid",
+    "GUI",
+    "Environment",
+    "build_manhattan_reward_function",
+    "find_target_position",
+]
 
